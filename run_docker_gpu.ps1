4a6acd0e562eba561d5f998a1c7e6fc9637abb51
JWebmeister <jwebmeister@gmail.com>
2023-12-30 15:03:18 +1100

    update run_docker_gpu to use gpus

--- run_docker_gpu.ps1
+++ run_docker_gpu.ps1
@@ -1,1 +1,1 @@
-docker run -it --rm -v ${pwd}:/mnt/input -w /mnt/input jwebmeister/kaldi_ag_training_gpu $args
+docker run --gpus all -it --rm -v ${pwd}:/mnt/input -w /mnt/input jwebmeister/kaldi_ag_training_gpu $args
